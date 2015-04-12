e8eb6f53d4ada92461535e57d040b7483fc80d68
thamywl <thamywl@gmail.com>
2015-04-12 18:34:52 +0800

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -41,7 +41,7 @@ with(data2,{
         points(Timestamp,Sub_metering_2, type="l", col = "red")
         points(Timestamp,Sub_metering_3, type="l", col = "blue")
         legend("topright", lty = 1,  col = c("black","red","blue")
-               , legend = c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3")
+               , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
                , bty = "n")
         plot(Timestamp, Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
 }) 
